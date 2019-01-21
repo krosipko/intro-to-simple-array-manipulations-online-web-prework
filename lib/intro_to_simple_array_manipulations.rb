ed9b3c1dd2bd1c8c966694612701a1d7ac41e2eb
Kyle Rosipko <krosipko@outlook.com>
2019-01-21 07:44:35 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,59 @@
+def using_push(arg1, arg2)
+  result = arg1
+  result.push(arg2)
+end
+
+def using_unshift(arg1, arg2)
+  result = arg1
+  result.unshift(arg2)
+end
+
+def using_pop(arg1)
+  result = arg1
+  result.pop
+end
+
+def pop_with_args(arg)
+  result = arg
+  result.pop(2)
+end
+
+def using_shift(arg)
+  result = arg
+  result.shift
+end
+
+def shift_with_args(arg)
+  result = arg
+  result.shift(2)
+end
+
+def using_concat(arg1, arg2)
+  result = arg1
+  result.concat(arg2)
+end
+
+def using_insert(arg1, arg2)
+  result = arg1
+  result.insert(4, arg2)
+end
+
+def using_uniq(arg)
+  result = arg
+  result.uniq
+end
+
+def using_flatten(arg)
+  result = arg
+  result.flatten
+end
+
+def using_delete(arg1, arg2)
+  result = arg1
+  result.delete(arg2)
+end
+
+def using_delete_at(arg1, arg2)
+  result = arg1
+  result.delete_at(arg2)
+end
